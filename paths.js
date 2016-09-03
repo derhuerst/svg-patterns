bd57a892efd455dbd181bc7c22df92482060690d
Jannis R <mail@jannisr.de>
2016-09-03 18:41:52 +0200

    more paths

--- paths.js
+++ paths.js
@@ -9,24 +9,51 @@ const pattern = require('./pattern')
 
 const round = (x) => Math.round(x * 1000) / 1000
 
-const path = (parts, close) =>
-	parts.map((part) => part[0] + round(part[1]) + ',' + round(part[2]))
-	.join('')
+const path = (parts, close) => parts
+	.map((p) => 'number' === typeof p ? Math.round(p * 1000) / 1000 : p)
+	.join(' ')
 	+ (close ? 'z' : '')
 
+// credit to @riccardoscalco!
+// taken from https://github.com/riccardoscalco/textures/blob/ca09566cb9e2dd0bf572639a7e17a9a96717c5e1/textures.coffee#L278-L306
+
 const squares = (s) => path([
-	, ['M', s/4, s/4]
-	, ['l', s/2, 0], ['l', 0, s/2], ['l', -s/2, 0]
+	'M',s/4,s/4,
+	'l',s/2,0, 'l',0,s/2, 'l',-s/2,0
 ], true)
 
 const nylon = (s) => path([
-	  ['M', 0, s/4], ['l', s/4, 0], ['l', 0, -s/4]
-	, ['M', s*3/4, s], ['l', 0, -s/4], ['l', s/4, 0]
-	, ['M', s/4, s/2], ['l', 0, s/4], ['l', s/4, 0]
-	, ['M', s/2, s/4], ['l', s/4, 0], ['l', 0, s/4]
-], false)
+	'M',0,s/4, 'l',s/4,0, 'l',0,-s/4,
+	'M',s*3/4,s, 'l',0,-s/4, 'l',s/4,0,
+	'M',s/4,s/2, 'l',0,s/4, 'l',s/4,0,
+	'M',s/2,s/4, 'l',s/4,0, 'l',0,s/4
+])
 
-const styles = {squares, nylon}
+// slightly adapted
+const waves = (s) => path([
+	'M',0,s/2,
+	'c',s/8,-s/4,s*3/8,-s/4,s/2,0,
+	'c',s/8,s/4,s*3/8,s/4,s/2,0
+])
+
+const woven = (s) => path([
+	'M',s/4,s/4, 'l',s/2,s/2,
+	'M',s*3/4,s/4, 'l',s/2,-s/2,
+	'M',s/4,s*3/4, 'l',-s/2,s/2,
+	'M',s*3/4,s*5/4, 'l',s/2,-s/2,
+	'M',-s/4,s/4, 'l',s/2,-s/2
+])
+
+const crosses = (s) => path([
+	'M',s/4,s/4, 'l',s/2,s/2,
+	'M',s/4,s*3/4, 'l',s/2,-s/2
+])
+
+const caps = (s) => path([
+	'M',s/4,s*3/4, 'l',s/4,-s/2, 'l',s/4,s/2
+])
+
+const styles = {squares, nylon, waves, woven, crosses, caps, hexagons}
 
 
 
