a4823abd087f3d71035d2355c6f95d4fc471e9a1
Jannis R <mail@jannisr.de>
2016-09-05 20:38:11 +0200

    interactivity

---
+++ demo.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const stringify = require('virtual-dom-stringify')
+const patterns = require('./index.js')
+
+
+
+const type = document.querySelector('#demo-type')
+const opt = document.querySelector('#demo-options')
+const defs = document.querySelector('#demo-target defs')
+
+const defaults = {
+	id: 'demo-pattern',
+	size: 20
+}
+
+const render = () => {
+	const t = type.value.split(',')
+	const o = Object.assign(defaults, JSON.parse(opt.value))
+
+	const pattern = t.length === 1 ? patterns[t[0]](o) : patterns[t[0]](t[1], o)
+	defs.innerHTML = stringify(pattern)
+}
+
+type.addEventListener('change', render)
+opt.addEventListener('change', render)
+opt.addEventListener('keypress', () => setTimeout(render, 5))
+setTimeout(render, 0)
